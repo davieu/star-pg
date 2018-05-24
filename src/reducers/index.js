fd7c85e3da90f42918a00578d2cd65ba360a89db
davieu <davisumana@gmail.com>
2018-05-24 17:03:25 -0400

    added import to reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import ListsReducer from "./reducer-lists";
 import OptionsReducer from "./reducer-lists";
+import SpeciesAttr from "../data/specie_attrs.js";
 
 
 const rootReducer = combineReducers({
