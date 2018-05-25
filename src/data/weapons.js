c0967502657d71f04b748882e416483512133030
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-05-25 12:54:07 -0400

    worked on weapons a little

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -1,22 +1,22 @@
-const Weapons = {
-  "Section": [
-    {
-      "type": "Ranged weapons",
-      "weapons": ["Blasters (DH-17 blaster pistol)",
-        "Blaster rifle (E-11 blaster rifle)",
-        "Heavy blaster pistol (DL-44 heavy blaster pistol)",
-        "Sporting blaster (Defender sporting blaster)",
-        "Light repeating blaster (T-21 light repeating blaster)",
-        "E-Web repeating blaster",
-        "Jawa ionization blaster",
-        "Bowcaster",
-        "Beam tube (Emperor-4 beam tube)",
-        "Ssi-ruuvi paddle beamer",
-        "Disruptor (DX-2 disruptor pistol, DXR-6 disruptor rifle)",
-        "Sonic pistol (Sd-77 sonic pistol, Sg-82 sonic rifle)",
-        "Wrist rockets (MM9 rocket system)",
-        "Stokhli spray stick",
-        "Projectile weapons (Oriolanis Striker Projectile Pistol, Dressellian projectile rifle)",
+const Weapons = {[
+    "E-11 blaster rifle",
+    "DL-44 heavy blaster pistol",
+    "Defender sporting blaster",
+    "T-21 light repeating blaster",
+    "E-Web repeating blaster",
+    "Jawa ionization blaster",
+    "Bowcaster",
+    "Beam tube (Emperor-4 beam tube)",
+    "Ssi-ruuvi paddle beamer",
+    "DXR-6 disruptor rifle",
+    "Sg-82 sonic rifle",
+  ]
+}
+      
+
+      "Wrist rockets (MM9 rocket system)",
+      "Stokhli spray stick",
+      "Projectile weapons (Oriolanis Striker Projectile Pistol, Dressellian projectile rifle)",
       "Flame projector (C-22 flame carbine, CR-24 flame rifle, ZX miniature flame projector)",
       "Dart shooter (Stealth-2VX palm shooter, PRP-502 hold-out dart shooter)",
       "Flechette launcher (FWG-5 flechette pistol, FC-1 flechette launcher)",
